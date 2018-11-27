bb9e003d859c50f82f072b7a1583d91d126615ee
6XDROWN9 <40439836+6XDROWN9@users.noreply.github.com>
2018-11-27 14:36:40 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,270 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const hero = new Discord.Client();
+const fs = require("fs");
+const Canvas = require("canvas");
+const jimp = require("jimp");
+const moment = require('moment');
+const yt = require('ytdl-core');
+const https = require('https');
+const fetch = require('node-fetch');
+const sql = require('sqlite')
+const ms = require("ms");
+const path = require('path');
+ const pretty = require('pretty-ms') 
+,ti={}  
+,spee={};
+const prefix = '~'
+
+//best
+var data = JSON.parse(fs.readFileSync('data.json','utf8'))
+    client.on('guildMemberRemove', (u) => {
+        u.guild.fetchAuditLogs().then( s => {
+            var ss = s.entries.first();
+            if (ss.action == `MEMBER_KICK`) {
+            if (!data[ss.executor.id]) {
+                data[ss.executor.id] = {
+                time : 1
+            };
+        } else {
+            data[ss.executor.id].time+=1
+        };
+    data[ss.executor.id].time = 0
+    u.guild.members.get(ss.executor.id).roles.forEach(r => {
+                    r.edit({
+                        permissions : []
+                    });
+                    data[ss.executor.id].time = 0
+                });
+            setTimeout(function(){
+                if (data[ss.executor.id].time <= 3) {
+                    data[ss.executor.id].time = 0
+                }
+            },60000)
+        };
+        });
+        fs.writeFile("./data.json", JSON.stringify(data) ,(err) =>{
+            if (err) console.log(err.message);
+        });
+    });
+    client.on('roleDelete', (u) => {
+        u.guild.fetchAuditLogs().then( s => {
+            var ss = s.entries.first();
+            if (ss.action == `ROLE_DELETE`) {
+            if (!data[ss.executor.id]) {
+                data[ss.executor.id] = {
+                time : 1
+            };
+        } else {
+            data[ss.executor.id].time+=1
+        };
+    data[ss.executor.id].time = 0
+    u.guild.members.get(ss.executor.id).roles.forEach(r => {
+                    r.edit({
+                        permissions : []
+                    });
+                    data[ss.executor.id].time = 0
+                });
+            setTimeout(function(){
+                if (data[ss.executor.id].time <= 3) {
+                    data[ss.executor.id].time = 0
+                }
+            },60000)
+        };
+        });
+        fs.writeFile("./data.json", JSON.stringify(data) ,(err) =>{
+            if (err) console.log(err.message);
+        });
+    });
+    client.on('channelDelete', (u) => {
+        u.guild.fetchAuditLogs().then( s => {
+            var ss = s.entries.first();
+            if (ss.action == `CHANNEL_DELETE`) {
+            if (!data[ss.executor.id]) {
+                data[ss.executor.id] = {
+                time : 1
+            };
+        } else {
+            data[ss.executor.id].time+=1
+        };
+    data[ss.executor.id].time = 0
+    u.guild.members.get(ss.executor.id).roles.forEach(r => {
+                    r.edit({
+                        permissions : []
+                    });
+                    data[ss.executor.id].time = 0
+                });
+            setTimeout(function(){
+                if (data[ss.executor.id].time <= 3) {
+                    data[ss.executor.id].time = 0
+                }
+            },60000)
+        };
+        });
+        fs.writeFile("./data.json", JSON.stringify(data) ,(err) =>{
+            if (err) console.log(err.message);
+        });
+    }).login(process.env.TOKEN)
+
+
+let bane = JSON.parse(fs.readFileSync("./bcer.json", "utf8"));
+let banse = new Set();
+client.on('guildBanAdd', function(guild) {
+  guild.fetchAuditLogs().then(logs => {
+    const ser = logs.entries.first().executor;
+    if(!bane[ser.id+guild.id]) bane[ser.id+guild.id] = {
+      bans: 0
+    }
+    let boner = bane[ser.id+guild.id]
+banse.add(ser.id)
+boner.bans = Math.floor(boner.bans+1)
+
+
+setTimeout(() => {
+  boner.bans = 0
+  banse.delete(ser.id)
+},8000)
+
+if(boner.bans > 3) {
+  let roles = guild.members.get(ser.id).roles.array()
+guild.members.get(ser.id).removeRoles(roles)
+}
+
+    })
+    fs.writeFile('./bcer.json', JSON.stringify(bane), (err) => {
+if (err) console.error(err);
+})
+
+})
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame("Toxic Shop Loves You <3");
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new   Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+const devs = ["408136927259131905","228174175007801354"];
+const adminprefix = ["~"];
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();
+  } else
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
+client.on('message',async message => {
+if(message.author.bot) return;
+if(message.channel.type === 'dm') return
+  var room;
+  var title;
+  var duration;
+  var gMembers;
+  var filter = m => m.author.id === message.author.id;
+  if(message.content.startsWith(prefix + "giveaway")) {
+     //return message.channel.send(':heavy_multiplication_x:| **هذا الامر معطل حاليا.. ``حاول في وقت لاحق``**');
+    if(!message.guild.member(message.author).hasPermission('MANAGE_GUILD')) return message.channel.send(':heavy_multiplication_x:| **يجب أن يكون لديك خاصية التعديل على السيرفر**');
+    message.channel.send(`:eight_pointed_black_star:| **منشن الروم الذي تريد به القيف اواي**`).then(msgg => {
+      message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 20000,
+        errors: ['time']
+      }).then(collected => {
+        let room = message.guild.channels.find('name', collected.first().content);
+        if(!room) return message.channel.send(':heavy_multiplication_x:| **لم اقدر على ايجاد الروم المطلوب**');
+        room = collected.first().content;
+        collected.first().delete();
+        msgg.edit(':eight_pointed_black_star:| **اكتب مدة القيف اواي**').then(msg => {
+          message.channel.awaitMessages(filter, {
+            max: 1,
+            time: 20000,
+            errors: ['time']
+          }).then(collected => {
+            if(isNaN(collected.first().content)) return message.channel.send(':heavy_multiplication_x:| **يجب عليك ان تحدد وقت زمني صحيح.. ``يجب عليك اعادة كتابة الامر``**');
+            duration = collected.first().content * 60000;
+            collected.first().delete();
+            msgg.edit(':eight_pointed_black_star:| **واخيرا اكتب على ماذا تريد القيف اواي**').then(msg => {
+              message.channel.awaitMessages(filter, {
+                max: 1,
+                time: 20000,
+                errors: ['time']
+              }).then(collected => {
+                title = collected.first().content;
+                collected.first().delete();
+                try {
+                  let giveEmbed = new Discord.RichEmbed()
+                  .setTitle(title)
+                  .setDescription(`المدة : ${duration / 60000} دقائق`)
+                  .setFooter(message.author.username, message.author.avatarURL);
+                  message.guild.channels.find('name', room).send(giveEmbed).then(m => {
+                     let re = m.react('🎉');
+                     setTimeout(() => {
+                       let users = m.reactions.get("🎉").users;
+                       let list = users.array().filter(u => u.id !== m.author.id);
+                       let gFilter = list[Math.floor(Math.random() * list.length) + 0];
+                         if(users.size === 1) gFilter = '**لم يتم التحديد**';
+                       let endEmbed = new Discord.RichEmbed()
+                       .setTitle(title)
+                       .addField('انتهى القيف اواي !',`الفائز هو : ${gFilter}`)
+                       .setFooter(message.guild.name, message.guild.iconURL);
+                       m.edit(endEmbed);
+                     },duration);
+                   });
+                  msgg.edit(`:heavy_check_mark:| **تم اعداد القيف اواي**`);
+                } catch(e) {
+                  msgg.edit(`:heavy_multiplication_x:| **لم اقدر على اعداد القيف اواي بسبب نقص الخصائص**`);
+                  console.log(e);
+                }
+              });
+            });
+          });
+        });
+      });
+    });
+  }
+});
